3c1ce809f52a09b06ada8d8121a47d5c494ea687
Brie McNally <brienne.mcnally@gmail.com>
2018-03-15 16:26:43 -0700

    Finish Javascript exercises

--- skeleton/phase_6_students_and_courses.js
+++ skeleton/phase_6_students_and_courses.js
@@ -9,6 +9,10 @@ Student.prototype.name = function() {
 };
 
 Student.prototype.enroll = function(course) {
+  if (this.hasConflict(course)) {
+    throw 'This course is in conflict with existing course.';
+  }
+
   if (!this.courses.includes(course)) {
     this.courses.push(course);
     course.students.push(this);
@@ -16,13 +20,24 @@ Student.prototype.enroll = function(course) {
 };
 
 Student.prototype.courseLoad = function() {
-  let hash = new Map();
+  // let hash = new Map();
+  // for (let course of this.courses) {
+  //   let credit = hash.get(course.department) || 0;
+  //   hash.set(course.department, credit + course.numCredits);
+  // }
+  let hash = {};
   for (let course of this.courses) {
-    let credit = hash.get(course.department) || 0;
-    hash.set(course.department, credit + course.numCredits);
+    let credit = hash[course.department] || 0;
+    hash[course.department] = credit + course.numCredits;
   }
 
   return hash;
+};
+
+Student.prototype.hasConflict = function(course) {
+  return this.courses.some(function(currentCourse) {
+    return currentCourse.conflictWith(course);
+  });
 };
 
 function Course(name, department, numCredits, daysOfWeek, timeBlock) {
@@ -39,18 +54,23 @@ Course.prototype.addStudent = function(student) {
 };
 
 Course.prototype.conflictWith = function(course) {
-  
+  if (this.timeBlock !== course.timeBlock) return false;
+  return this.daysOfWeek.some(function(day) {
+    return course.daysOfWeek.includes(day);
+  });
 };
 
 
 let student1 = new Student('John', 'Smith');
 let student2 = new Student('Walter', 'Wan');
 
-let course1 = new Course('math', 'math', 5);
-let course2 = new Course('american history 4', 'history', 6);
+let course1 = new Course('math', 'math', 5, ['monday', 'wendnesday', 'friday'], 3);
+let course2 = new Course('american history 4', 'history', 6, ['monday', 'sunday'], 3);
 
 student1.enroll(course1);
 student1.enroll(course2);
-student1.enroll(course2);
+
 
 console.log(student1.courseLoad());
+
+console.log(course1.conflictWith(course2));
